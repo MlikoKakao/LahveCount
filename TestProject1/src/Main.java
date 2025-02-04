5743ce80c868aceeb28a35d3f168fbc1667dc783
MlikoKakao <85055283+MlikoKakao@users.noreply.github.com>
2025-02-04 13:01:45 +0100

    Everything except Export

--- TestProject1/src/Main.java
+++ TestProject1/src/Main.java
@@ -10,52 +10,92 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String akce;
+		String konec = "";
 		
+		
+		
+		ArrayList<Lahev> lahve = new ArrayList<Lahev>();
+	do {
 		System.out.println("Zápis lahví");
 		System.out.println("Jakou akci chcete provést?:");
 		System.out.println("P - Přidat, S - smazat, E - export do text., "
-				+"AVGw - průměr s DPH, \nAVGo - průměr bez DPH,"
-				+ " AVGl - průměrný objem láhve");
-		akce = scanner.next();
-		ArrayList<ArrayList<String>> seznamLahvi = new ArrayList<ArrayList
-		<String>>();
-		ArrayList<String> lahve = new ArrayList<String>();
-	
+							+"AVGw - průměr s DPH, \nAVGo - průměr bez DPH,"
+							+ " AVGl - průměrný objem láhve");
+		String akce = scanner.next().toUpperCase();
 	switch(akce) {
 	case("P"): 
-		Lahev myObj = new Lahev(akce, akce, 0, 0, 0);
-	System.out.println(myObj.getName());
-	System.out.println(myObj.getBarva());
-	System.out.println(myObj.getMl());
-	System.out.println(myObj.getCena());
-	System.out.println(myObj.getDph());
-	
-	lahve.add(akce);
-	System.out.println(lahve);
+		System.out.print("Napiš název lahve: ");	
+		String tName = scanner.next();
+		System.out.print("Napiš barvu lahve: ");	
+		String tBarva = scanner.next();
+		System.out.print("Napiš kolik má lahev ml: ");	
+		int tMl = scanner.nextInt();
+		System.out.print("Napiš jaká je cena lahve: ");	
+		int tCena = scanner.nextInt();
+		System.out.print("Napiš jaké je DPH: ");	
+		int tDph = scanner.nextInt();
+			
+		Lahev myObj = new Lahev(tName, tBarva, tMl, tCena, tDph);
+			System.out.println("Název: "+myObj.getName()+", Barva: "
+					+ myObj.getBarva()+", Ml: "+myObj.getMl() +", Cena: "+myObj.getCena()+
+					", DPH: "+myObj.getDph());
+			
+			lahve.add(myObj);
+			System.out.println(lahve);
 		break;
+	case("S"):
+		lahve.remove(scanner.nextInt());
+	case("E"):
+	case("AVGW"):
+		System.out.println("Průměrná cena je: "+Average(lahve));
+		break;
+	case("AVGO"):
+		System.out.println("Průměrná cena je: "+ AverageWithout(lahve));
+		break;
+	case("AVGL"):
+		System.out.println("Průměrné ml je: " + AverageMl(lahve));
+		break;
+			
 		
-		
-		
-		/*String nazev1;
-		String ml1;
-		System.out.print("Přidej název lahve: ");
-		nazev1 = scanner.next();
-		System.out.print("Kolik má ml?: ");
-		ml1 = scanner.next();
-		lahve.add(nazev1);
-		lahve.add(ml1);
-		seznamLahvi.add(lahve);
-		System.out.println(lahve);
-	
-		
-		System.out.println(seznamLahvi);
-		System.out.println("Smaž lahev: ");
-		seznamLahvi.remove(scanner.nextInt());
-		scanner.close();
-		System.out.println(seznamLahvi);*/
 		default: 
 			System.out.println("Nothing happened");
 	}
+	System.out.print("Napiš Konec pokud chceš končit: ");
+	konec = scanner.next();
+	} while(!konec.equalsIgnoreCase("konec"));
 	}
+	
+	public static double Average(ArrayList<Lahev> lahve) {
+		double prumer=0;
+		double sum = 0;
+		int count = 0;
+		for (Lahev lahev : lahve) {
+			sum += lahev.getCena();
+			count ++;
+			prumer = sum / count;
+		}		
+		return prumer;
+	}
+	public static double AverageWithout(ArrayList<Lahev> lahve) {
+		double prumerBez=0;
+		double sum = 0;
+		int count = 0;
+		for (Lahev lahev : lahve) {
+			sum += lahev.getCena()-lahev.getDph();
+			count ++;
+			prumerBez = sum / count;
+		}		
+		return prumerBez;
+	}
+	public static double AverageMl(ArrayList<Lahev> lahve) {
+		double prumerMl=0;
+		double sum = 0;
+		int count = 0;
+		for (Lahev lahev : lahve) {
+			sum += lahev.getMl();
+			count ++;
+			prumerMl = sum / count;
+		}		
+		return prumerMl;
+}
 }
