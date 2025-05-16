9409838dd8391d6e912a74f1516ace253268093a
PC <mattewczlp@seznam.cz>
2025-05-16 13:58:27 +0200

    Příprava na zkoušku

--- LahveCount-main/TestProject1/src/Main.java
+++ LahveCount-main/TestProject1/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String konec = "";
+		String konec;
 
 
 		ArrayList<Lahev> lahve = new ArrayList<>();
